1b3e184fa393fdf7673da65b08987066fef91487
Karol <karol.jadczyk11@gmail.com>
2025-05-08 19:33:53 +0200

    Dodano --logs i -l

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-if [[ "$1" == "--date" || "$1" == "-d" ]]; then
-    date
+if [[ "$1" == "--logs" || "$1" == "-l" ]]; then
+    count=${2:-100}
+    for i in $(seq 1 $count); do
+        echo "log$i.txt | Skrypt: $0 | Data: $(date)" > log$i.txt
+    done
 fi
